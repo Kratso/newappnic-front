e9b942b7c7bfb2c79c6dd42981bf49d47aeed0f3
Kratso <kratsothelsamar@gmail.com>
2022-12-28 10:55:36 +0100

    feat: 🎸 added basic helpers for stats

---
+++ src/helpers/getOwedAmount.ts
@@ -0,0 +1,26 @@
+import { Concepto } from "../slices/concepto.slice";
+import { User } from "../slices/login.slice";
+
+export function getAmountOwed(conceptos: Concepto[], user: User): number {
+    let amountOwed = 0;
+  
+    for (const concepto of conceptos) {
+      const participante = (concepto.participantes as any).find(
+        (p: { usuario: User; pagado: boolean } | string) =>
+          typeof p === "object" && p.usuario._id === user._id
+      );
+  
+      if (participante) {
+        // If the user is a participant in this concepto and has not yet paid,
+        // add their share of the cost to the amount owed
+        if (typeof participante === "object" && !participante.pagado) {
+          amountOwed += concepto.precio * concepto.unidades / concepto.participantes.length;
+        }
+      } else if ((concepto.pagador as any)._id === user._id) {
+        // If the user is the pagador for this concepto, add the total cost to the amount owed
+        amountOwed -= concepto.precio * concepto.unidades;
+      }
+    }
+  
+    return amountOwed;
+  }
---
+++ src/helpers/getViajeTotals.ts
@@ -0,0 +1,30 @@
+import { Concepto } from "../slices/concepto.slice";
+import { Viaje } from "../slices/viajes.slice";
+
+export const getPrecioTotal = (conceptos: Concepto[]) => {
+    return conceptos.reduce((acc, curr) => {
+        return acc + curr.precio*curr.unidades;
+    }, 0);   
+}
+
+export const getPrecioPerCapita = (conceptos: Concepto[]) => {
+    return conceptos.reduce((acc, curr) => {
+        return acc + curr.precio*curr.unidades/curr.participantes.length;
+    }, 0);   
+}
+
+export const getPrecioPerDiem = (viaje: Viaje, conceptos: Concepto[]) => {
+    const dias = (new Date(viaje.end_date).getTime() - new Date(viaje.start_date).getTime()) / (1000 * 3600 * 24);
+
+    return conceptos.reduce((acc, curr) => {
+        return acc + curr.precio*curr.unidades/dias;
+    }, 0);
+}
+
+export const getPrecioPerDiemPerCapita = (viaje: Viaje, conceptos: Concepto[]) => {
+    const dias = (new Date(viaje.end_date).getTime() - new Date(viaje.start_date).getTime()) / (1000 * 3600 * 24);
+
+    return conceptos.reduce((acc, curr) => {
+        return acc + curr.precio*curr.unidades/dias/curr.participantes.length;
+    }, 0);
+}
