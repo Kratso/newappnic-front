2dcb7740f03942b01b661abd5ae7b9375de86616
Kratso <kratsothelsamar@gmail.com>
2023-06-16 20:05:20 +0200

    feat: 🎸 added frontpage protection

--- src/components/ViajesList/ViajesList.tsx
+++ src/components/ViajesList/ViajesList.tsx
@@ -1,27 +1,36 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { selectUserRole } from "../../slices/login.slice";
+import { selectUser, selectUserRole, User } from "../../slices/login.slice";
 import { selectAllViajes } from "../../slices/viajes.slice";
 import { RootState } from "../../store/store";
 import { Container, Stack } from "@mui/system";
 import { ViajeItem } from "./components/ViajeItem/ViajeItem";
 import ViajeFormDialog from "../ViajeForm/ViajeForm";
+import { useMemo } from "react";
 
 const ViajesList = () => {
   const viajes = useSelector((state: RootState) => selectAllViajes(state));
-  const userRole = useSelector((state: RootState) => selectUserRole(state));
+  const user = useSelector((state: RootState) => selectUser(state));
+
+  const userRole = useMemo(() => user.user?.role, [user]);
+  const viajesOfUser = useMemo(
+    () =>
+      viajes.filter(
+        (viaje) =>
+          (viaje.participantes as any).find(
+            (participante: User) => participante._id === user.user?._id
+          ) || userRole === "admin"
+      ),
+    [user.user?._id, userRole, viajes]
+  );
 
   return (
     <Container>
       <Stack spacing={2}>
-        {viajes.map((viaje, i) => (
+        {viajesOfUser.map((viaje, i) => (
           <ViajeItem key={i} viaje={viaje} />
         ))}
       </Stack>
-      {userRole === "admin" ? (
-        <ViajeFormDialog fab={true} />
-      ) : (
-        <></>
-      )}
+      {userRole === "admin" ? <ViajeFormDialog fab={true} /> : <></>}
     </Container>
   );
 };
