fa714e827b6a5687a795ec18c325fec4262caca0
Kratso <kratsothelsamar@gmail.com>
2023-09-13 18:28:46 +0200

    chore(release): 0.1.12

--- src/helpers/getOwedAmount.ts
+++ src/helpers/getOwedAmount.ts
@@ -1,28 +1,44 @@
 import { Concepto } from "../slices/concepto.slice";
 import { User } from "../slices/login.slice";
 
-export function getAmountOwed(conceptos: Concepto[], user: User, isContable: Boolean): number {
-    let amountOwed = 0;
-  
-    for (const concepto of conceptos) {
-      const participante = (concepto.participantes as any).find(
-        (p: { usuario: User; pagado: boolean } | string) =>
-          typeof p === "object" && p.usuario._id === user._id
-      );
-  
-      if (participante) {
-        // If the user is a participant in this concepto and has not yet paid,
-        // add their share of the cost to the amount owed
-        if (typeof participante === "object" && !participante.pagado) {
-          amountOwed += concepto.precio * concepto.unidades / concepto.participantes.length;
-        }
-      }
-      if ((concepto.pagador as any)._id === user._id && !isContable) {
-        const debtUsers = (concepto.participantes as any[]).reduce((acc, p) => p.pagado ? acc : ++acc, 0);
-        // If the user is the pagador for this concepto, add the total cost to the amount owed
-        amountOwed -= ((concepto.participantes as any[]).find((p: any) => p.usuario._id === user._id).pagado ? 0 : 1)*(concepto.precio * concepto.unidades) * debtUsers / concepto.participantes.length;
+export function getAmountOwed(
+  conceptos: Concepto[],
+  user: User,
+  isContable: Boolean
+): number {
+  let amountOwed = 0;
+
+  for (const concepto of conceptos) {
+    const participante = (concepto.participantes as any).find(
+      (p: { usuario: User; pagado: boolean } | string) =>
+        typeof p === "object" && p.usuario._id === user._id
+    );
+
+    if (participante) {
+      // If the user is a participant in this concepto and has not yet paid,
+      // add their share of the cost to the amount owed
+      if (typeof participante === "object" && !participante.pagado) {
+        amountOwed +=
+          (concepto.precio * concepto.unidades) / concepto.participantes.length;
       }
     }
-  
-    return amountOwed;
+    if ((concepto.pagador as any)._id === user._id && !isContable) {
+      const debtUsers = (concepto.participantes as any[]).reduce(
+        (acc, p) => (p.pagado ? acc : ++acc),
+        0
+      );
+      // If the user is the pagador for this concepto, add the total cost to the amount owed
+      amountOwed -=
+        (((concepto.participantes as any[]).find(
+          (p: any) => p.usuario._id === user._id
+        )?.pagado
+          ? 0
+          : 1) *
+          (concepto.precio * concepto.unidades) *
+          debtUsers) /
+        concepto.participantes.length;
+    }
   }
+
+  return amountOwed;
+}
